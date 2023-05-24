cb2f9c85042bdeae90fa0bc5c3212a0d377564c9
shehab sadek <shehab.m.sadek@gmail.com>
2023-05-24 18:11:22 +0300

    changes

---
+++ README.md
@@ -0,0 +1,3 @@
+# Datasets
+MNAD: https://www.kaggle.com/datasets/jmourad100/mnad-moroccan-news-articles-dataset
+AND : https://www.kaggle.com/datasets/asmaaabdelwahab/arabic-news-dataset
--- TextHandler.py
+++ TextHandler.py
@@ -1,25 +1,43 @@
-import re
+import pyarabic.araby as araby
+from nltk.stem.isri import ISRIStemmer
 from pyarabic.araby import strip_tashkeel
-from pyarabic.araby import tokenize
 from nltk.corpus import stopwords
+import re
+
+def remove_english_text(texts):
+    arabic_texts = []
+    english_pattern = re.compile(r'[a-zA-Z]')
+    
+    for text in texts:
+        arabic_text = re.sub(english_pattern, '', text)
+        arabic_texts.append(arabic_text)
+    
+    return arabic_texts
 
 def preprocess_text(words):
     preprocessed_text = []
     
     stop_words = set(stopwords.words('arabic'))
     
+    stemmer = ISRIStemmer()
+    words=remove_english_text(words)
     for text in words:
-        # Replace variations of 'و' (or) with a space
-        text = re.sub(r'\bو\b', ' ', text)
+        # Tokenize the text
+        tokens = araby.tokenize(text)
         
-        tokens = tokenize(text)
+        # Filter out English words
+        arabic_tokens = [token for token in tokens if not token.isascii()]
         
-        filtered_tokens = [token for token in tokens if token not in stop_words]
+        # Apply stemming and disambiguation
+        stemmed_tokens = [stemmer.stem(token) for token in arabic_tokens]
         
-        filtered_tokens = [token for token in filtered_tokens if token.isalpha()]
+        # Filter out stopwords and non-alphabetic tokens
+        filtered_tokens = [token for token in stemmed_tokens if token not in stop_words and token.isalpha()]
         
+        # Remove diacritics from tokens
         filtered_tokens = [strip_tashkeel(token) for token in filtered_tokens]
         
+        # Join the preprocessed tokens
         preprocessed_text.append(' '.join(filtered_tokens))
     
     return preprocessed_text
