1089ca191003482d4349304511dff5c9f644d80d
shehab sadek <shehab.m.sadek@gmail.com>
2023-05-25 15:48:03 +0300

    changes

--- TextHandler.py
+++ TextHandler.py
@@ -2,36 +2,34 @@ import pyarabic.araby as araby
 from nltk.stem.isri import ISRIStemmer
 from pyarabic.araby import strip_tashkeel
 from nltk.corpus import stopwords
-import re
+
+def ortho_normalize(text):
+    text = araby.normalize_alef(text)
+    text = araby.normalize_teh(text)
+    return text
 
 def remove_english_text(texts):
     arabic_texts = []
-    english_pattern = re.compile(r'[a-zA-Z]')
-    
     for text in texts:
-        arabic_text = re.sub(english_pattern, '', text)
+        arabic_tokens = [token for token in araby.tokenize(text) if araby.is_arabicrange(token)]
+        arabic_text = ' '.join(arabic_tokens)
         arabic_texts.append(arabic_text)
-    
     return arabic_texts
 
-def preprocess_text(words):
+def preprocess_text(texts):
     preprocessed_text = []
-    
-    stop_words = set(stopwords.words('arabic'))
-    
     stemmer = ISRIStemmer()
-    words=remove_english_text(words)
-    for text in words:
-        tokens = araby.tokenize(text)
-        
-        arabic_tokens = [token for token in tokens if not token.isascii()]
-    
+    stop_words = set(stopwords.words('arabic'))
+
+    texts = remove_english_text(texts)
+
+    for text in texts:
+        text = araby.strip_tashkeel(text)
+        text = ortho_normalize(text)
+        tokens = text.split()
+        arabic_tokens = [token for token in tokens if araby.is_arabicrange(token)]
         stemmed_tokens = [stemmer.stem(token) for token in arabic_tokens]
-        
         filtered_tokens = [token for token in stemmed_tokens if token not in stop_words and token.isalpha()]
-        
-        filtered_tokens = [strip_tashkeel(token) for token in filtered_tokens]
-        
         preprocessed_text.append(' '.join(filtered_tokens))
     
     return preprocessed_text
